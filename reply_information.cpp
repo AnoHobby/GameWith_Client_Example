889e18629d09ea351e563fe57ad8228524fb47b9
AnoHobby <96468681+AnoHobby@users.noreply.github.com>
2024-12-15 16:55:22 +0900

    Add files via upload

---
+++ reply_information.cpp
@@ -0,0 +1,16 @@
+#if defined(__INTELLISENSE__)
+#include "http.cpp";//github.com/anohobby/Cpp_Http_Library
+#else
+import http;
+#endif
+#include <iostream>
+int main() {
+	http::Query_Parameter param{
+	{L"game_id",L"0000"},//game id
+	{L"number",L"000000"},//>>000000
+	};
+	//std::wcout << param.to_string();
+	http::Client client(L"https://gamewith.jp/api/search/bbs/game/post/show",param.to_string());
+	std::cout<<client.request<http::methods::GET>().read_content();
+	return 0;
+}	
